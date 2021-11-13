b8ee60bd09628b9c52e3ff7a76d1cf58cc41d62d
taki hasan <takihasan@takis-MacBook-Air.local>
2021-11-13 08:19:45 +0530

    Added Enviroment and initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+require("dotenv").config();
+const express=require('express');
+const bodyParser=require('body-parser');
+const ejs=require('ejs');
+const mongoose=require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+
+
+mongoose.connect('mongodb://127.0.0.1/userDB');
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields: ["password"]});
+
+const User= new mongoose.model("User",userSchema);
+
+const app= express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extender:true
+}));
+
+
+
+app.get("/", (req,res)=> {
+    res.render("home.ejs");
+})
+
+app.get("/login", (req,res)=> {
+    res.render("login");
+})
+
+app.get("/register", (req,res)=> {
+    res.render("register");
+})
+
+app.post("/register", (req,res)=> {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+ 
+    newUser.save(err => {
+        if(err){console.log(err);}else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login", (req,res)=> {
+    const username=req.body.username;
+    const password=req.body.password;
+
+    User.findOne({email:username}, (err,foundUser)=> {
+        if(err){console.log(err);}else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+
+            }
+        }
+    });
+});
+
+
+
+app.listen(3000, ()=> {
+    console.log("server is up and running on port 3000");
+});
